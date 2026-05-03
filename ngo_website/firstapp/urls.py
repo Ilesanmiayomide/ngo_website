cc88b58bee7fab29e75306afd83935ed9e75947c
ILesanmiayomide <ayomideilesanmi505@gmail.com>
2026-05-03 16:57:07 +0100

    payment integration

--- ngo_website/firstapp/urls.py
+++ ngo_website/firstapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('programs/', views.programs, name='programs'),
+
 ]
--- ngo_website/firstapp/views.py
+++ ngo_website/firstapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
+
+PAYPAL_CLIENT_ID = "your_sandbox_client_id"
+PAYPAL_SECRET = "your_sandbox_secret"
+PAYPAL_BASE_URL = "https://api-m.sandbox.paypal.com"
 # Create your views here.
 
 from django.shortcuts import render
@@ -28,3 +33,69 @@ def contact(request):
 
 def programs(request):
     return render(request, 'programs.html')
+
+
+
+PAYPAL_CLIENT_ID = "AWqyMA_Z0XW4OjpnyotbkOd1CBZAXpE9zEFPAfG0btqabAelBtfHjQlOcwhN6OPOgDhtDFagW70-sVz8"
+PAYPAL_SECRET = "EE8V02eG6_-ZkNIhxF8r-WB2MKGYrfzyD2jvvWoE5x_AFQJQGk8YrhVkjWkHmW2abjTyPVgyErEEPTVp"
+PAYPAL_BASE_URL = "https://api-m.sandbox.paypal.com"
+
+
+def get_access_token():
+    response = requests.post(
+        f"{PAYPAL_BASE_URL}/v1/oauth2/token",
+        auth=(PAYPAL_CLIENT_ID, PAYPAL_SECRET),
+        headers={"Accept": "application/json"},
+        data={"grant_type": "client_credentials"},
+    )
+    return response.json()["access_token"]
+
+
+def create_donation(request):
+    access_token = get_access_token()
+
+    order_data = {
+        "intent": "CAPTURE",
+        "purchase_units": [
+            {
+                "amount": {
+                    "currency_code": "USD",
+                    "value": "10.00"
+                }
+            }
+        ],
+        "application_context": {
+            "return_url": "http://localhost:8000/paypal/success/",
+            "cancel_url": "http://localhost:8000/paypal/cancel/"
+        }
+    }
+
+    response = requests.post(
+        f"{PAYPAL_BASE_URL}/v2/checkout/orders",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}"
+        },
+        json=order_data
+    )
+
+    data = response.json()
+
+    # extract approval link
+    for link in data["links"]:
+        if link["rel"] == "approve":
+            return redirect(link["href"])
+        
+def paypal_success(request):
+    token = request.GET.get("token")
+    access_token = get_access_token()
+
+    response = requests.post(
+        f"{PAYPAL_BASE_URL}/v2/checkout/orders/{token}/capture",
+        headers={
+            "Authorization": f"Bearer {access_token}",
+            "Content-Type": "application/json"
+        }
+    )
+
+    return HttpResponse("Payment successful")
